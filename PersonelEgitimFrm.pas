3ee9e557114196e90e0b7c47359836db8c92f1cf
Cmptr\Cmptr <a_kadir53@hotmail.com>
2017-10-21 10:12:25 +0300

    personel eğitimleri formu safha 2 derleme tamamlandı

--- PersonelEgitimFrm.pas
+++ PersonelEgitimFrm.pas
@@ -132,23 +132,13 @@ begin
   dateEdit.ValueTip := tvString;
   setDataStringKontrol(self,dateEdit, 'BaslamaTarihi','Baþlama Tarihi',Kolon1,'',100);
 
+  setDataString(self,'BaslamaSaati','Baþlama Saati',Kolon1,'',100);
+
   dateEdit := TcxDateEditKadir.Create(self);
   dateEdit.ValueTip := tvString;
   setDataStringKontrol(self,dateEdit, 'BitisTarihi','Bitiþ Tarihi',Kolon1,'',100);
 
-
-BaslamaSaati
-
-Sure
-Egitimci
-EgitimTuru
-EgitimYeri
-SertifikaNo
-EgitimIcerigi
-EgitimUcreti
-EgitimUcretParaBirimi
-EgitimUcretiOdendi
-
+  setDataString(self,'Sure','Süre (Saat)',Kolon1,'',100);
   kombo := TcxImageComboKadir.Create(self);
   kombo.Conn := Datalar.ADOConnection2;
   kombo.TableName := 'DoktorlarT';
@@ -157,62 +147,24 @@ EgitimUcretiOdendi
   kombo.BosOlamaz := True;
   kombo.Filter := '';
   OrtakEventAta(kombo);
-  setDataStringKontrol(self,kombo,'Doktor','Doktor',kolon1,'',120);{}
+  setDataStringKontrol(self,kombo,'Egitimci','Eðitimci',kolon1,'',120);{}
+  //setDataString(self,'Egitimci','Eðitimci',Kolon1,'',100);
+  setDataStringC(self,'EgitimTuru','Eðitim Türü',Kolon1,'',100, 'Ýç Eðitim,Dýþ Eðitim,Diðer');
+  setDataString(self,'EgitimYeri','Eðitim Yeri',Kolon1,'',100);
+  setDataString(self,'SertifikaNo','Sertifika No.',Kolon1,'',100);
+  setDataString(self,'EgitimIcerigi','Eðitim Ýçeriði',Kolon1,'',100);
+  setDataString(self,'EgitimUcreti','Eðitim Ücreti',Kolon1,'',100);
+  setDataString(self,'EgitimUcretParaBirimi','Para Birimi',Kolon1,'',100);
 
+  kombo := TcxImageComboKadir.Create(self);
+  kombo.Conn := nil;
+  kombo.BosOlamaz := True;
+  kombo.ItemList := '0;Hayýr,1;Evet';
+  kombo.Filter := '';
+  OrtakEventAta(kombo);
+  setDataStringKontrol(self,kombo,'EgitimUcretiOdendi','Ödendi mi?',kolon1,'',120);
 
-  setDataStringB(self,'kod','Doktor Kodu',Kolon1,'',70,List,True,nil);
-
-  setDataString(self,'tanimi','Adý Soyadý',Kolon1,'',200,True);
-  setDataString(self,'tcKimlikNo','TC Kimlik No',Kolon1,'',150,True);
-
-  bransKodu := TcxImageComboKadir.Create(self);
-  bransKodu.Conn := Datalar.ADOConnection2;
-  bransKodu.TableName := 'SERVIS_TNM';
-  bransKodu.ValueField := 'kod';
-  bransKodu.DisplayField := 'tanimi';
-  bransKodu.BosOlamaz := True;
-  bransKodu.Filter := '';
-  OrtakEventAta(bransKodu);
-  setDataStringKontrol(self,bransKodu,'bransKodu','Branþ Kodu',kolon1,'',120);{}
-
-
-
-  setDataString(self,'tescilNo','Tescil No',Kolon1,'',80,True);
-  setDataString(self,'eReceteKullanici','Reçete Kullanýcý Adý',Kolon1,'cc',100,True);
-  setDataString(self,'eReceteSifre','Reçete Þifresi',Kolon1,'',100,True);
-  setDataString(self,'pin','Ýmza Token Pin',Kolon1,'',50);
-  setDataString(self,'TesisKodu','Tesis Kodu',Kolon1,'',100);
-  setDataString(self,'GSM','GSM',Kolon1,'',100);
-  setDataString(self,'EPosta','E-Posta',Kolon1,'',200);
- // setDataString(self,'TDisID','TDis ID',Kolon1,'TDIS',80);
- // addButton(self,nil,'btnTDis','','TDIS ID Getir',Kolon1,'TDIS',120,ButtonClick);
-  setDataString(self,'sertifika','Sertifika',Kolon1,'',80);
-
-
-  calismaTipi := TcxImageComboKadir.Create(self);
-  calismaTipi.Conn := nil;
-  calismaTipi.BosOlamaz := True;
-  calismaTipi.ItemList := '0;Tam Zamanlý,1;Yarý Zamanlý';
-  calismaTipi.Filter := '';
-  OrtakEventAta(calismaTipi);
-  setDataStringKontrol(self,calismaTipi,'calismaTipi','Çalýþma Tipi',kolon1,'',120);
-   {}
-  setDataStringC(self,'uzman','Uzman mý?',Kolon1,'',80,'Evet,Hayýr');
-  setDataStringC(self,'durum','Durum',Kolon1,'',80,'Aktif,Pasif');
-
-  setDataStringBLabel(self,'CalismaBilgisi',sayfa2_Kolon1,'',290,'Seans Çalýþma Bilgisi');
-  TcxLabel(FindComponent('labelCalismaBilgisi')).Properties.Alignment.Horz := taCenter;
-  setDataStringC(self,'pazartesi','Pazatesi',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'sali','Salý',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'carsamba','Çarþamba',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'persembe','Perþembe',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'cuma','Cuma',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'cumartesi','Cumartesi',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-
- // setDataImage(self,'foto','Foto',Kolon2,'',120,100);
-
-  SayfaCaption('Taným Bilgileri','Çalýþma Bilgileri','','','');
-
+  setDataStringC(self,'','',Kolon1,'',100, 'Evet,Hayýr');
 
   Disabled(self,True);
   _HastaBilgileriniCaptionGoster_ := True;
