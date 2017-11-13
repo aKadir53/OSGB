28f93290f4a667a811fa7fa5f60465ff705e955b
Cmptr\Cmptr <a_kadir53@hotmail.com>
2017-11-13 15:18:28 +0300

    wewer

--- About_Frm.pas
+++ About_Frm.pas
@@ -8,7 +8,8 @@ uses
   cxContainer, cxEdit, Menus, StdCtrls, cxButtons, cxGroupBox, DB, ADODB,
   cxTextEdit, cxMaskEdit, cxButtonEdit, cxDBEdit,kadirType,KadirLabel,Kadir,  GirisUnit,Data_Modul, dxSkinsCore, dxSkinBlue, dxSkinCaramel, dxSkinCoffee,
   dxSkiniMaginary, dxSkinLilian, dxSkinLiquidSky, dxSkinLondonLiquidSky,
-  dxSkinMcSkin, dxSkinMoneyTwins, dxSkinsDefaultPainters, cxCheckBox, cxLabel;
+  dxSkinMcSkin, dxSkinMoneyTwins, dxSkinsDefaultPainters, cxCheckBox, cxLabel,
+  Vcl.Imaging.jpeg, Vcl.ExtCtrls;
 
 
 
@@ -28,3 +29,4 @@ implementation
 
 {$R *.dfm}
 end.
+
