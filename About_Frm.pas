743648baa750b879a42b090915aafafb118556ad
Cmptr\Cmptr <a_kadir53@hotmail.com>
2017-11-14 10:34:19 +0300

    girisUnit'de formdaki mevcut Image nesnesini layout'a atama özelliği eklendi GirisUnit'de warning ve hint temizliği yapıldı. bunlar yapılırken görülen try / finally ihtiyaçlarına müdahale edildi. aboutfrm değişiklikleri

--- About_Frm.pas
+++ About_Frm.pas
@@ -8,12 +8,14 @@ uses
   cxContainer, cxEdit, Menus, StdCtrls, cxButtons, cxGroupBox, DB, ADODB,
   cxTextEdit, cxMaskEdit, cxButtonEdit, cxDBEdit,kadirType,KadirLabel,Kadir,  GirisUnit,Data_Modul, dxSkinsCore, dxSkinBlue, dxSkinCaramel, dxSkinCoffee,
   dxSkiniMaginary, dxSkinLilian, dxSkinLiquidSky, dxSkinLondonLiquidSky,
-  dxSkinMcSkin, dxSkinMoneyTwins, dxSkinsDefaultPainters, cxCheckBox, cxLabel;
+  dxSkinMcSkin, dxSkinMoneyTwins, dxSkinsDefaultPainters, cxCheckBox, cxLabel,
+  Vcl.Imaging.jpeg, Vcl.ExtCtrls, cxImage;
 
 
 
 type
   TfrmAbout = class(TGirisForm)
+    Image1abx: TcxImage;
     procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
@@ -63,10 +65,11 @@ begin
   List.Conn := Datalar.ADOConnection2;
   List.SkinName := 'coffee';//AnaForm.dxSkinController1.SkinName;
 
-  setDataStringB(self,'kod','Doktor Kodu',Kolon1,'',70,List,True,nil);
+  setDataImage(self,'Image1abx','Foto',Kolon1,'',155,121);
+  setDataStringB(self,'kod','Doktor Kodu',Kolon2,'',70,List,True,nil);
 
-  setDataString(self,'tanimi','Adý Soyadý',Kolon1,'',200,True);
-  setDataString(self,'tcKimlikNo','TC Kimlik No',Kolon1,'',150,True);
+  setDataString(self,'tanimi','Adý Soyadý',Kolon2,'',200,True);
+  setDataString(self,'tcKimlikNo','TC Kimlik No',Kolon2,'',150,True);
 
   bransKodu := TcxImageComboKadir.Create(self);
   bransKodu.Conn := Datalar.ADOConnection2;
@@ -76,20 +79,20 @@ begin
   bransKodu.BosOlamaz := True;
   bransKodu.Filter := '';
   OrtakEventAta(bransKodu);
-  setDataStringKontrol(self,bransKodu,'bransKodu','Branþ Kodu',kolon1,'',120);
+  setDataStringKontrol(self,bransKodu,'bransKodu','Branþ Kodu',Kolon2,'',120);
 
 
 
-  setDataString(self,'tescilNo','Tescil No',Kolon1,'',80,True);
-  setDataString(self,'eReceteKullanici','Reçete Kullanýcý Adý',Kolon1,'cc',100,True);
-  setDataString(self,'eReceteSifre','Reçete Þifresi',Kolon1,'',100,True);
-  setDataString(self,'pin','Ýmza Token Pin',Kolon1,'',50);
-  setDataString(self,'TesisKodu','Tesis Kodu',Kolon1,'',100);
-  setDataString(self,'GSM','GSM',Kolon1,'',100);
-  setDataString(self,'EPosta','E-Posta',Kolon1,'',200);
- // setDataString(self,'TDisID','TDis ID',Kolon1,'TDIS',80);
- // addButton(self,nil,'btnTDis','','TDIS ID Getir',Kolon1,'TDIS',120,ButtonClick);
-  setDataString(self,'sertifika','Sertifika',Kolon1,'',80);
+  setDataString(self,'tescilNo','Tescil No',Kolon2,'',80,True);
+  setDataString(self,'eReceteKullanici','Reçete Kullanýcý Adý',Kolon2,'cc',100,True);
+  setDataString(self,'eReceteSifre','Reçete Þifresi',Kolon2,'',100,True);
+  setDataString(self,'pin','Ýmza Token Pin',Kolon2,'',50);
+  setDataString(self,'TesisKodu','Tesis Kodu',Kolon2,'',100);
+  setDataString(self,'GSM','GSM',Kolon2,'',100);
+  setDataString(self,'EPosta','E-Posta',Kolon2,'',200);
+ // setDataString(self,'TDisID','TDis ID',Kolon2,'TDIS',80);
+ // addButton(self,nil,'btnTDis','','TDIS ID Getir',Kolon2,'TDIS',120,ButtonClick);
+  setDataString(self,'sertifika','Sertifika',Kolon2,'',80);
 
 
   calismaTipi := TcxImageComboKadir.Create(self);
@@ -98,21 +101,21 @@ begin
   calismaTipi.ItemList := '0;Tam Zamanlý,1;Yarý Zamanlý';
   calismaTipi.Filter := '';
   OrtakEventAta(calismaTipi);
-  setDataStringKontrol(self,calismaTipi,'calismaTipi','Çalýþma Tipi',kolon1,'',120);
+  setDataStringKontrol(self,calismaTipi,'calismaTipi','Çalýþma Tipi',Kolon2,'',120);
 
-  setDataStringC(self,'uzman','Uzman mý?',Kolon1,'',80,'Evet,Hayýr');
-  setDataStringC(self,'durum','Durum',Kolon1,'',80,'Aktif,Pasif');
+  setDataStringC(self,'uzman','Uzman mý?',Kolon2,'',80,'Evet,Hayýr');
+  setDataStringC(self,'durum','Durum',Kolon2,'',80,'Aktif,Pasif');
 
-  setDataStringBLabel(self,'CalismaBilgisi',sayfa2_Kolon1,'',290,'Seans Çalýþma Bilgisi');
+  setDataStringBLabel(self,'CalismaBilgisi',sayfa2_Kolon2,'',290,'Seans Çalýþma Bilgisi');
   TcxLabel(FindComponent('labelCalismaBilgisi')).Properties.Alignment.Horz := taCenter;
-  setDataStringC(self,'pazartesi','Pazatesi',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'sali','Salý',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'carsamba','Çarþamba',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'persembe','Perþembe',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'cuma','Cuma',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
-  setDataStringC(self,'cumartesi','Cumartesi',sayfa2_Kolon1,'',80,'0,1,1-2,1-3,2,2-3,3');
+  setDataStringC(self,'pazartesi','Pazatesi',sayfa2_Kolon2,'',80,'0,1,1-2,1-3,2,2-3,3');
+  setDataStringC(self,'sali','Salý',sayfa2_Kolon2,'',80,'0,1,1-2,1-3,2,2-3,3');
+  setDataStringC(self,'carsamba','Çarþamba',sayfa2_Kolon2,'',80,'0,1,1-2,1-3,2,2-3,3');
+  setDataStringC(self,'persembe','Perþembe',sayfa2_Kolon2,'',80,'0,1,1-2,1-3,2,2-3,3');
+  setDataStringC(self,'cuma','Cuma',sayfa2_Kolon2,'',80,'0,1,1-2,1-3,2,2-3,3');
+  setDataStringC(self,'cumartesi','Cumartesi',sayfa2_Kolon2,'',80,'0,1,1-2,1-3,2,2-3,3');
 
- // setDataImage(self,'foto','Foto',Kolon2,'',120,100);
+ // setDataImage(self,'foto','Foto',Kolon3,'',120,100);
 
   SayfaCaption('Taným Bilgileri','Çalýþma Bilgileri','','','');
 
