8e2112fffdcb215ae3ad8902266b3d1e72e1fc92
Cmptr\Cmptr <a_kadir53@hotmail.com>
2017-12-14 09:26:44 +0300

    1009 versiyonu

--- OSGB.dpr
+++ OSGB.dpr
@@ -55,7 +55,7 @@ uses
 // KadirMedula3 in '..\..\medula3wsdl\KadirMedula3.pas';
 
 const
-  AppalicationVer : integer = 1008;
+  AppalicationVer : integer = 1009;
   // Versiyon info kontrol etmeyi unutma
 
 {$R *.res}
