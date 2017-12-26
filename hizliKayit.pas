26c8fed34bf0054e8a5370dbc65f5539bc366dad
Cmptr\Cmptr <a_kadir53@hotmail.com>
2017-12-26 16:47:05 +0300

    başlangıç tarihi format çevrimi yapıldı alan sıralaması düzeltildi

--- hizliKayit.pas
+++ hizliKayit.pas
@@ -146,7 +146,7 @@ var
   bBasarili : Boolean;
   iCount : Integer;
   _row_ : integer;
-  Cins,Medeni,DTarih : String;
+  Cins,Medeni,DTarih,BTarih : String;
 begin
   try
     datalar.ADOConnection2.BeginTrans;
@@ -165,6 +165,9 @@ begin
         DTarih := ifThen(pos('.',GridList.Cells[12,_row_]) > 0,
                          NoktasizTarih(GridList.Cells[12,_row_]),GridList.Cells[12,_row_]);
 
+        BTarih := ifThen(pos('.',GridList.Cells[15,_row_]) > 0,
+                         NoktasizTarih(GridList.Cells[15,_row_]),GridList.Cells[15,_row_]);
+
         sql := Format(_insertPersonel_,
                       [QuotedStr(datalar.AktifSirket),
                        QuotedStr(GridList.Cells[1,_row_]),
@@ -180,8 +183,8 @@ begin
                        QuotedStr(GridList.Cells[11,_row_]),
                        QuotedStr(DTarih),
                        QuotedStr(GridList.Cells[13,_row_]),
-                       QuotedStr(GridList.Cells[14,_row_]),
-                       QuotedStr(GridList.Cells[15,_row_]),
+                       QuotedStr(BTarih),
+                       'NULL',
                        QuotedStr(datalar.username),
                        QuotedStr('1'),
                        QuotedStr(datalar.AktifSube)]);
