a6b6eb56c854808b9f8369553e40879136165da7
Cmptr2\Cmptr <a_kadir53@hotmail.com>
2017-12-29 02:15:53 +0300

    tc ile personel arama eklendi. bulıunamazsa tabı kapatıyor.

--- OSGB.dpr
+++ OSGB.dpr
@@ -55,7 +55,7 @@ uses
 // KadirMedula3 in '..\..\medula3wsdl\KadirMedula3.pas';
 
 const
-  AppalicationVer : integer = 1013;
+  AppalicationVer : integer = 1014;
   // Versiyon info kontrol etmeyi unutma
 
 {$R *.res}
