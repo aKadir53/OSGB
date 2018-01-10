a41a5d4c29e0917d438b6a627a7bb65155693297
Cmptr\Cmptr <a_kadir53@hotmail.com>
2018-01-10 15:35:38 +0300

    1027

--- OSGB.dpr
+++ OSGB.dpr
@@ -59,7 +59,7 @@ uses
 // KadirMedula3 in '..\..\medula3wsdl\KadirMedula3.pas';
 
 const
-  AppalicationVer : integer = 1026;
+  AppalicationVer : integer = 1027;
   // Versiyon info kontrol etmeyi unutma
 
 {$R *.res}
