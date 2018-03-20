b2c5ce8ed7644452c0278ad691def9cc1b54744d
Cmptr\Cmptr <a_kadir53@hotmail.com>
2018-03-21 01:23:07 +0300

    - sürüm 1057 için ön hazırlık

--- OSGB.dpr
+++ OSGB.dpr
@@ -64,7 +64,7 @@ uses
 // KadirMedula3 in '..\..\medula3wsdl\KadirMedula3.pas';
 
 const
-  AppalicationVer : integer = 1056;
+  AppalicationVer : integer = 1057;
   // Versiyon info kontrol etmeyi unutma
 
 {$R *.res}
